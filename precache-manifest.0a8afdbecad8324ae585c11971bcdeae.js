36fe39bd88b77dd2f45278b8e6fd80b56595aa89
[vivek] <[vivek.kch05@gmail.com]>
2020-06-05 16:50:37 +0530

    Updates

---
+++ precache-manifest.0a8afdbecad8324ae585c11971bcdeae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc0512c356ddff0ff4737b09f4915c8",
+    "url": "/Book-Store/index.html"
+  },
+  {
+    "revision": "28aa6634e2d1ed04df9d",
+    "url": "/Book-Store/static/css/main.c9a939aa.chunk.css"
+  },
+  {
+    "revision": "1af83e32ee77f7e21e0e",
+    "url": "/Book-Store/static/js/2.333f3277.chunk.js"
+  },
+  {
+    "revision": "3d3c77a9abac65e318cda84f3f64b401",
+    "url": "/Book-Store/static/js/2.333f3277.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28aa6634e2d1ed04df9d",
+    "url": "/Book-Store/static/js/main.748aa1fe.chunk.js"
+  },
+  {
+    "revision": "6bed551ef48b19bbdbec",
+    "url": "/Book-Store/static/js/runtime-main.fa152ecf.js"
+  }
+]);
