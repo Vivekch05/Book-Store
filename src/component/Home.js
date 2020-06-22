d6b76db361f7623a88077e96ebd413e50b26b6f1
[vivek] <[vivek.kch05@gmail.com]>
2020-06-22 15:09:46 +0530

    json update

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
-
+import Books from '../images/book1.jpg'
 const Home = () =>{
     return(
         <div style={{margin:"10px"}}>
             <h1>Home</h1>
-            <img src = {process.env.PUBLIC_URL + "/book1.jpg"}
+            <img src = {Books}
             alt="Books"
             display="flex"
             flexWrap="wrap"
             width="100%"
-            height="600px"
+            height="600px" 
             />
         </div>
     )
